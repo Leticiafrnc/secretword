9cdf4da87f8eb932096bad40f0849df9bc99fcce
Letícia Franco <leticiafrnc1@gmail.com>
2025-08-11 08:44:18 -0300

    escolhendo palavra e categoria de forma aleatoria

--- src/App.js
+++ src/App.js
@@ -17,13 +17,48 @@ const stages =[
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name)
   const [words] = useState(wordsList)
-  // iniciar o jogo
+
+  const [pickedWord, setPickedWord] = useState("")
+  const [pickedCategory, setPickedCategory] = useState("")
+  const [letters, setLetters] = useState([])
+  
+  const pickwordAndCategory = () => {
+    //escolha uma categoria aleatória
+  const categories = Object.keys(words)
+  const category = categories[Math.floor(Math.random() * categories.length)]
+  console.log(category)
+
+  //escolha uma palavra aleatória
+ const word = words[category][Math.floor(Math.random() * words[category].length)]
+ console.log(word)
+
+ return [word, category]
+
+}
+  // comece o jogo de palavras secretas
   const startGame = () => {
+    //escolha a palavra e escolha a categoria
+   const[word, category ] = pickwordAndCategory()
+
+   // pegar a palavra e transformar em letras 
+   let wordLetters = word.split("")
+   wordLetters = wordLetters.map(letter => letter.toLowerCase())
+   console.log(word, category)
+   console.log(wordLetters)
+
+   // set estados
+   setPickedWord(word)
+   setPickedCategory(category)
+   setLetters(letters)
+
     setGameStage(stages[1].name)
   }
+
+  //progredir na entrada da letra
   const verifyLetter = () =>{
     setGameStage(stages[2].name)
   }
+// reestartar o jogo
    const retry = () =>{
     setGameStage(stages[0].name)
   }
